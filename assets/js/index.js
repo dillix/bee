7f75d8c39d051b1a0e4d763ff52c7e3d3a6104d1
Achilles Kaloeridis <achilleas90gr@gmail.com>
2017-04-30 21:15:01 +0300

    Adds basic webpack config

---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+console.log('Works :D');
+
+import '../scss/styles.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,49 @@
-var path = require('path');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+let path = require('path');
 
 module.exports = {
   entry: './assets/js/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'js')
-  }
-};
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: "style-loader",
+          loader: "css-loader?sourceMap",
+        }),
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: "style-loader",
+          loader: "css-loader?sourceMap!sass-loader?sourceMap",
+        }),
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        exclude: /node_modules/,
+        loader: ["url-loader?limit=10000&name=images/[hash:12].[ext]"],
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+    ]
+  },
+  devtool:"source-map",
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    new CleanWebpackPlugin(['dist'])
+  ]
+}
